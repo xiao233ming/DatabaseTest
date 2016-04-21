fd3aa20594eb5608a163ebe5582a38e831e66b81
xiao233ming <dianziwanwan@163.com>
2016-04-21 22:51:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mingming/databasetest/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mingming.databasetest;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private MyDatabaseHelper dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new MyDatabaseHelper(this,"BookStore.db",null,2);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        Button addData = (Button) findViewById(R.id.add_data);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //开始组装第一条数据
+                values.put("name","The Da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",16.96);
+                db.insert("Book",null,values);//插入第一条数据
+                values.clear();
+
+                //开始组装第二条数据
+                values.put("name","The Lost Symbol");
+                values.put("author","Dan Brown");
+                values.put("pages",510);
+                values.put("price",16.95);
+                db.insert("Book",null,values);//插入第二条数据
+            }
+        });
+
+    }
+}
