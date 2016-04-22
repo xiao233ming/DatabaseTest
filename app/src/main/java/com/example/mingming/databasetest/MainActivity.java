eaff8be0d77c3cfe356d5be6b4e515d01a60a234
xiao233ming <dianziwanwan@163.com>
2016-04-22 20:05:20 +0800

    update

--- app/src/main/java/com/example/mingming/databasetest/MainActivity.java
+++ app/src/main/java/com/example/mingming/databasetest/MainActivity.java
@@ -2,30 +2,60 @@ package com.example.mingming.databasetest;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
     private MyDatabaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHelper = new MyDatabaseHelper(this,"BookStore.db",null,2);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
         Button createDatabase = (Button) findViewById(R.id.create_database);
         Button addData = (Button) findViewById(R.id.add_data);
         Button updateData = (Button) findViewById(R.id.update_data);
         Button deleteButton = (Button) findViewById(R.id.delete_data);
+        Button queryButton = (Button) findViewById(R.id.query_data);
+
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //查询Book表中所有的数据
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if(cursor.moveToFirst()){
+                    do{
+                        //遍历Cursor对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages  = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d(TAG,"book name is " + name);
+                        Log.d(TAG,"book author is" + author);
+                        Log.d(TAG,"book pages is" + pages);
+                        Log.d(TAG,"book price is" + pages);
+
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
+            }
+        });
 
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
-                db.delete("Book","pages > ?",new String[]{"500"});
+                db.delete("Book", "pages > ?", new String[]{"500"});
             }
         });
 
@@ -34,9 +64,10 @@ public class MainActivity extends Activity {
             public void onClick(View view) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
-                values.put("price",10.99);
-                db.update("Book",values,"name=?",new String[]{"" +
+                values.put("price", 10.99);
+                db.update("Book", values, "name=?", new String[]{"" +
                         "The Da Vinci Code"});
+
             }
         });
 
@@ -53,19 +84,19 @@ public class MainActivity extends Activity {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 ContentValues values = new ContentValues();
                 //开始组装第一条数据
-                values.put("name","The Da Vinci Code");
-                values.put("author","Dan Brown");
-                values.put("pages",454);
-                values.put("price",16.96);
-                db.insert("Book",null,values);//插入第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);//插入第一条数据
                 values.clear();
 
                 //开始组装第二条数据
-                values.put("name","The Lost Symbol");
-                values.put("author","Dan Brown");
-                values.put("pages",510);
-                values.put("price",16.95);
-                db.insert("Book",null,values);//插入第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 16.95);
+                db.insert("Book", null, values);//插入第二条数据
             }
         });
 
